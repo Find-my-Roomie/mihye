dfe00823e5673cdfe29346a469a0bc97428f21a3
namihye <naming238@ewhain.net>
2021-02-04 15:20:17 +0900

    git

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, {Component, useState} from 'react'
+import Lander from './components/Test/Lander';
+
+function App() {
+  return (
+    <Lander></Lander>
+  );
+}
+
+export default App;
